987681e756245f8cace473a3783af4fa18bb0543
Kyle Ong <kho226@nyu.edu>
2019-02-14 15:54:52 -0500

    writing to files, etc.

--- src/sessionization.py
+++ src/sessionization.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from parser import Parser
+import csv
 
 
 class Sessionizer(object):
@@ -21,24 +22,36 @@ class Sessionizer(object):
     def is_expired(self, timedelta):
         return timedelta >= self.inactivity_period
     
-    def insert_record(self,**kwargs):
-        start_time = kwargs.get("start_time")
+    
+    def upsert_in_record(self, **kwargs):
+        start_time = kwargs.get("start_time", None)
+        end_time = kwargs.get("end_time", None)
         ip = kwargs.get("ip")
         cik = kwargs.get("cik")
         accession = kwargs.get("accession")
         extention = kwargs.get("extention")
 
         address = "{}{}{}".format(cik, accession, extention)
-
         val = self.in_record.get(ip, {})
-        
-        val["start_time"] = start_time
-
         visited = val.get("visited", set())
         visited.add(address)
-        val["visited"] = visited 
+        val["visited"] = visited
 
+        if val.get("start_time" , None) is None:
+            val["start_time"] = start_time
+        
         self.in_record[ip] = val
+    
+    def insert_out_record(self, **kwargs):
+        ip = kwargs.get("ip")
+        elapsed_time = kwargs.get("elapsed_time")
+        val = self.in_record.get(ip, {})
+        start_time = val.get("start_time", -1)
+        end_time = start_time + elapsed_time if elapsed_time < self.inactivity_period else \
+            start_time + self.inactivity_period
+        val["end_time"] = end_time
+        self.out_record[ip] = val
+        
 
     def parse(self):
         count = 0
@@ -53,33 +66,49 @@ class Sessionizer(object):
                 accession = self.p.accession(s)
                 extention = self.p.extention(s)
                 current_time = datetime.strptime("{}{}".format(date,time), "%Y-%m-%d%H:%M:%S")
-                if self.have_seen(ip):
-                    #update  record
-                    val = self.in_record.get(ip, {})
-                    visited = val.get("visited", set())
-                    visited.add("{}{}{}".format(cik, accession, extention))
-                else:
-                    val = self.in_record(ip, {})
-                    #insert record
-                    start_time = val.get("start_time", -1)
-                    start_time = current_time
-                    val["start_time"] = start_time
 
-                    visited = val.get("visited", set())
-                    visted.add("{}{}{}".format(cik,accession,extention))
-                    val["visited"] = visited 
+                self.upsert_record(
+                        start_time = current_time,
+                        ip = ip,
+                        cik = cik,
+                        accession = accession,
+                        extention = extention)
+    
                 elapsed_time = current_time - self.in_record.get("start_time")
                 if self.is_expired(elapsed_time):
-                    # insert_record
-                    val = self.in_record.get(ip, -1)
-                    start_time = val.get("start_time", -1)
-                    end_time = start_time + elapsed_time if elapsed_time < self.inactivity_period else \
-                            start_time + self.inactivity_period
-                    val["end_time"] = end_time
-                    self.out_record[ip] = val
-
-
+                    self.insert_out_record(
+                        ip=ip,
+                        elapsed_time = elapsed_time
+                    )
+        self.terminated_open_sesssions()
+        self.calculate_visits()
+        self.write_to_file()
                 
-
+    def terminated_open_sesssions(self):
+        for k, v in self.in_record.items():
+            if k not in self.out_record:
+                self.insert_out_record(
+                    ip = k,
+                    elapsed_time = self.inactivity_period
+                )
+    
+    def calculate_visits(self):
+        for k, v in self.out_record.items():
+            duration = v.get("end_time", 0) - v.get("start_time", 0)
+            self.out_record[k]["duration"] = duration
+    
+    def write_to_file(self):
+        with open(self.outfile, "w") as fh:
+            writer = csv.DictWriter(fh, field_names=["ip_address", "start_time", "end_time", "duration", "count"], delimiter=",")
+            writer.writeheader()
+            for k, v in self.out_record.items():
+                ip = k
+                start_time = v["start_time"]
+                end_time = v["end_time"]
+                duration = v["duration"].seconds
+                count = len(v["visited"])
+                out_line = "{}{}{}{}{}".format(start_time, end_time, duration, count)
+                fh.write(out_line)
 
     
+                    
--- src/test_sessionization.py
+++ src/test_sessionization.py
@@ -28,15 +28,17 @@ class sessionizationTestCase(TestCase):
         test = self.s.is_expired(timedelta(seconds=2))
         self.assertEqual(test, True)
     
-    def test_insert_record(self):
+    def test_upsert_record(self):
         start_time = datetime.now()
+        end_time = datetime.now() + timedelta(seconds = 10)
         ip = "127.0.0.01"
         cik  = "1"
         accession = "2"
         extention = "3"
 
-        self.s.insert_record(
+        self.s.upsert_in_record(
             start_time = start_time,
+            end_time = end_time,
             ip = ip,
             cik = cik,
             accession = accession,
@@ -44,12 +46,74 @@ class sessionizationTestCase(TestCase):
         )
         expected = {
             "start_time" : start_time,
-            "visited" : set({"{}{}{}".format(cik, accession, extention)})
+            "visited" : set({"{}{}{}".format(cik, accession, extention)}),
+            "end_time" : end_time
         }
         self.assertEqual(self.s.in_record[ip]["visited"],expected["visited"])
         self.assertEqual(self.s.in_record[ip]["start_time"], expected["start_time"])
     
+    def test_insert_out_record(self):
+        elapsed_time = timedelta(hours=1)
+        start_time = datetime.now()
+        end_time = datetime.now() + timedelta(seconds = 10)
+        ip = "127.0.0.01"
+        cik  = "1"
+        accession = "2"
+        extention = "3"
+        self.s.upsert_in_record(
+            start_time = start_time,
+            end_time = end_time,
+            ip = ip,
+            cik = cik,
+            accession = accession,
+            extention = extention
+        )
+        self.s.insert_out_record(
+            elapsed_time = elapsed_time,
+            ip = ip
+        )
+        self.assertEquals(
+            self.s.out_record[ip]["end_time"],
+            self.s.in_record[ip]["start_time"])
+    
+    def test_terminate_open_sessions(self):
+        elapsed_time = timedelta(hours=1)
+        start_time = datetime.now()
+        end_time = datetime.now() + timedelta(seconds = 10)
+        ip = "127.0.0.01"
+        cik  = "1"
+        accession = "2"
+        extention = "3"
+        self.s.upsert_in_record(
+            start_time = start_time,
+            end_time = end_time,
+            ip = ip,
+            cik = cik,
+            accession = accession,
+            extention = extention
+        )
+        self.s.terminated_open_sesssions()
+        self.assertEqual(self.s.out_record[ip]["end_time"], start_time)
 
+    def test_calculate_visits(self):
+        elapsed_time = timedelta(hours=1)
+        start_time = datetime.now()
+        end_time = datetime.now() + timedelta(seconds = 10)
+        ip = "127.0.0.01"
+        cik  = "1"
+        accession = "2"
+        extention = "3"
+        self.s.upsert_in_record(
+            start_time = start_time,
+            end_time = end_time,
+            ip = ip,
+            cik = cik,
+            accession = accession,
+            extention = extention
+        )
+        self.s.terminated_open_sesssions()
+        self.s.calculate_visits()
+        self.assertEquals(self.s.out_record[ip]["duration"], timedelta(0))
 
 if __name__ == "__main__":
     unittest.main()
